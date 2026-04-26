93233434c391197f8706fcca78ef4959bdea7e20
Eph23 <ephraims.818@gmail.com>
2026-04-26 16:49:51 +0600

    REACT--ULTIMATE-COURSE - State Events Forms - [PROJECT] - 04. Controlled Elements

--- 02. State/travel-list/src/App.jsx
+++ 02. State/travel-list/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
     { id: 1, description: "Passports", quantity: 2, packed: false },
     { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -13,15 +15,38 @@ function Logo() {
     );
 }
 function Form() {
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
     function handleSubmit(event) {
         event.preventDefault();
-        
+
+        if (!description) return;
+
+        const newItem = {
+            description,
+            quantity,
+            packed: false,
+            id: Date.now(),
+        };
+        console.log(newItem);
+
+        setDescription("");
+        setQuantity(1);
+    }
+    
+    function handleItem(event) {
+        setDescription(event.target.value);
+    }
+
+    function handleQuantity(event) {
+        setQuantity(+event.target.value);
     }
 
     return (
         <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your trip?</h3>
-            <select name="" id="">
+            <select onChange={handleQuantity} value={quantity}>
                 {Array.from({ length: 20 }, (_, index) => index + 1).map(
                     (num) => (
                         <option value={num} key={num}>
@@ -30,7 +55,12 @@ function Form() {
                     ),
                 )}
             </select>
-            <input type="text" placeholder="Items..." />
+            <input
+                type="text"
+                placeholder="Items..."
+                value={description}
+                onChange={handleItem}
+            />
             <button>Add</button>
         </form>
     );
