0a9b6db2b832dcd47d2e16d7365b8b08e74177df
Eph23 <ephraims.818@gmail.com>
2026-04-29 02:12:26 +0600

    REACT--ULTIMATE-COURSE - State Management - 05. Sorting Items

--- 03. State Management/travel-list/src/App.jsx
+++ 03. State Management/travel-list/src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function Logo() {
     return (
         <header>
@@ -60,10 +65,32 @@ function Form({ onAddItems }) {
     );
 }
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+    const [sortBy, setSortBy] = useState("input");
+
+    function handleSort(event) {
+        setSortBy(event.target.value);
+    }
+
+    let sortedItems;
+
+    if (sortBy === "input") {
+        sortedItems = items;
+    }
+    if (sortBy === "description") {
+        sortedItems = items
+            .slice()
+            .sort((a, b) => a.description.localeCompare(b.description));
+    }
+    if (sortBy === "packed") {
+        sortedItems = items
+            .slice()
+            .sort((a, b) => Number(a.packed) - Number(b.packed));
+    }
+
     return (
         <div className="list">
             <ul>
-                {items.map((item) => (
+                {sortedItems.map((item) => (
                     <Item
                         key={`${item.id}${item.description}`}
                         onDeleteItem={onDeleteItem}
@@ -72,6 +99,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
                     />
                 ))}
             </ul>
+            <div className="actions">
+                <select value={sortBy} onChange={handleSort}>
+                    <option value="input">Sort by input order</option>
+                    <option value="description">Sort by description</option>
+                    <option value="packed">Sort by packed status</option>
+                </select>
+            </div>
         </div>
     );
 }
@@ -119,7 +153,7 @@ function Status({ items }) {
 }
 
 function App() {
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState(initialItems);
 
     function handleAddItems(item) {
         setItems((items) => [...items, item]);
