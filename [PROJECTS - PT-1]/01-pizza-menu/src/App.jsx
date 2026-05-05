7c715e6ced5fddbb3bb20d96ca02710a266ce79f
Eph23 <ephraims.818@gmail.com>
2026-05-05 15:13:11 +0600

    REACT--ULTIMATE-COURSE - SECTION PROJECTS - PT1 - Pizza Menu -- 03. Applying Conditinals

--- [PROJECTS - PT-1]/01-pizza-menu/src/App.jsx
+++ [PROJECTS - PT-1]/01-pizza-menu/src/App.jsx
@@ -1,67 +1,109 @@
 import PIZZAS from "./data.js";
-
 function Header() {
     return (
         <header className="header">
-            <h1>Fast React Pizza</h1>
+            {" "}
+            <h1>Fast React Pizza</h1>{" "}
         </header>
     );
 }
-
 function Menu() {
     return (
         <div className="menu">
-            <h2>Our Menu</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Sapiente repudiandae sunt quas accusamus et officiis dicta
-                voluptas quae perspiciatis eveniet incidunt explicabo, inventore
-                aliquam modi?
-            </p>
+            {" "}
+            {PIZZAS.length > 0 ? (
+                <>
+                    {" "}
+                    <h2>Our Menu</h2>{" "}
+                    <p>
+                        {" "}
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Sapiente repudiandae sunt quas accusamus et officiis
+                        dicta voluptas quae perspiciatis eveniet incidunt
+                        explicabo, inventore aliquam modi?{" "}
+                    </p>{" "}
+                </>
+            ) : (
+                <p>
+                    {" "}
+                    <p>
+                        {" "}
+                        We are still working on our menu. Please come back
+                        later{" "}
+                    </p>{" "}
+                </p>
+            )}{" "}
         </div>
     );
 }
-
 function Pizzas() {
     return (
         <ul className="pizzas">
+            {" "}
             {PIZZAS.map((pizza) => (
                 <Pizza key={pizza.name} pizza={pizza} />
-            ))}
+            ))}{" "}
         </ul>
     );
 }
-
 function Pizza({ pizza }) {
     return (
-        <li className="pizza">
-            <img src={pizza.photoName} alt="" />
+        <li className={`pizza ${pizza.soldOut ? "sold-out" : ""}`}>
+            {" "}
+            <img src={pizza.photoName} alt="" />{" "}
             <div>
-                <h3>{pizza.name}</h3>
-                <p>{pizza.ingredients}</p>
-                <span>{pizza.price}</span>
-            </div>
+                {" "}
+                <h3>{pizza.name}</h3> <p>{pizza.ingredients}</p>{" "}
+                <span>{pizza.soldOut ? "SOLD OUT" : pizza.price}</span>{" "}
+            </div>{" "}
         </li>
     );
 }
-
 function Footer() {
+    const openHour = 12;
+    const closeHour = 22;
+    const currentHour = new Date().getHours();
+    const isOpen = currentHour >= openHour && currentHour <= closeHour;
+    console.log(isOpen);
     return (
         <footer className="footer">
-            <p>We are happy to welcome you between 12:00 and 12:00</p>
+            {" "}
+            {isOpen ? (
+                <OrderOnline openHour={openHour} closeHour={closeHour} />
+            ) : (
+                <StoreNotice closeHour={closeHour} />
+            )}{" "}
         </footer>
     );
 }
-
-function App() {
+function StoreNotice({ openHour, closeHour }) {
     return (
-        <div className="container">
-            <Header />
-            <Menu />
-            <Pizzas />
-            <Footer />
+        <p>
+            {" "}
+            We are happy to welcome you between {openHour}:00 and {closeHour}
+            :00{" "}
+        </p>
+    );
+}
+function OrderOnline({ closeHour }) {
+    return (
+        <div className="order">
+            {" "}
+            <p>
+                {" "}
+                We are OPEN until {closeHour}:00. Come visit us or order
+                online{" "}
+            </p>{" "}
+            <button className="btn">Order Online</button>{" "}
         </div>
     );
 }
-
+function App() {
+    return (
+        <div className="container">
+            {" "}
+            <Header /> <Menu /> <Pizzas /> <Footer />{" "}
+        </div>
+    );
+}
 export default App;
