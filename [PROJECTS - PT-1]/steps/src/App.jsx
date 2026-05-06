7c97326558a7de59479170598e327afb803e76d8
Eph23 <ephraims.818@gmail.com>
2026-05-07 04:56:49 +0600

    REACT--ULTIMATE-COURSE - SECTION PROJECTS - PT1 - Steps -- 02. Adding Event Listener

--- [PROJECTS - PT-1]/steps/src/App.jsx
+++ [PROJECTS - PT-1]/steps/src/App.jsx
@@ -1,17 +1,45 @@
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
+
 function App() {
+    let step = 1;
+
+    function handleNext() {
+        console.log("Clicked Next");
+    }
+
+    function handlePrevious() {
+        console.log("Clicked Previous");
+    }
+
     return (
         <div className="steps">
             <div className="numbers">
-                <div className="active">1</div>
-                <div className="">2</div>
-                <div className="">3</div>
+                <div className={step >= 1 ? "active" : ""}>1</div>
+                <div className={step >= 2 ? "active" : ""}>2</div>
+                <div className={step >= 3 ? "active" : ""}>3</div>
             </div>
             <div className="message">
-                <p>Step 1: "Learn React ⚛️"</p>
+                <p>
+                    Step {step}: {messages[step - 1]}
+                </p>
             </div>
             <div className="buttons">
-                <button>Previous</button>
-                <button>Next</button>
+                <button
+                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                    onClick={handlePrevious}
+                >
+                    Previous
+                </button>
+                <button
+                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                    onClick={handleNext}
+                >
+                    Next
+                </button>
             </div>
         </div>
     );
