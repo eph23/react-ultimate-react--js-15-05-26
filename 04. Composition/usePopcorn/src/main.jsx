181f02c8ca02b507d15bac26174dce565402551a
Eph23 <ephraims.818@gmail.com>
2026-05-18 22:23:51 +0600

    REACT--ULTIMATE-COURSE - Components Composition Reusability - 09. Improving Reusability With Props

--- 04. Composition/usePopcorn/src/main.jsx
+++ 04. Composition/usePopcorn/src/main.jsx
@@ -1,12 +1,29 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 /* import './index.css'
 import App from './App.jsx' */
 import StartRating from "./StartRating";
 
+function Test() {
+    const [movieRating, setMovieRating] = useState();
+
+    return (
+        <div>
+            <StartRating
+                color="blue"
+                maxRating={10}
+                onSetRating={setMovieRating}
+            />
+            <p>This movie was rated {movieRating} stars</p>
+        </div>
+    );
+}
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         {/* <App /> */}
         <StartRating maxRating={5} />
+        <StartRating size={24} color="red" defaultRating={3} />
+        <Test />
     </StrictMode>,
 );
